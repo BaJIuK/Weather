969c7994009b1d9582b258fcc96f7770a9b7f1ab
KoiDev <sp1rtwork@gmail.com>
2017-01-17 16:58:57 +0100

    created models for api

--- app/src/main/java/com/bajiuk/weather/api/WeatherApi.java
+++ app/src/main/java/com/bajiuk/weather/api/WeatherApi.java
@@ -1,10 +1,17 @@
 package com.bajiuk.weather.api;
 
 import com.bajiuk.weather.api.model.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface WeatherApi {
-  Observable<Response> getByLatLng(double longitude, double latitude);
 
-  Observable<Response> getByName(String cityName);
+  public static final String BASE_URL = "api.openweathermap.org/data/2.5/";
+
+  @GET("weather")
+  Observable<Response> getByLatLng(@Query("lat") double longitude, @Query("lon") double latitude);
+
+  @GET("weather")
+  Observable<Response> getByName(@Query("id") String cityName);
 }
---
+++ app/src/main/java/com/bajiuk/weather/api/di/WeatherApiModule.java
@@ -0,0 +1,16 @@
+package com.bajiuk.weather.api.di;
+
+import com.bajiuk.weather.api.WeatherApi;
+import dagger.Provides;
+import retrofit2.Retrofit;
+
+/**
+ * @author KoiDev
+ * @email DevSteelKoi@gmail.com
+ */
+
+public class WeatherApiModule {
+  @Provides WeatherApi provideApi() {
+    // TODO
+  }
+}
